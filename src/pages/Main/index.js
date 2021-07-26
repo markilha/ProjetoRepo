0c270296e5da9c5a2c9a727d664f2d980f0d9b1c
Markilha <markilha@gmail.com>
2021-07-26 15:17:51 -0300

    Estilizando pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Title} from './styles';
+import { Container, Form,SumitButton } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa'
 
-export default function Main(){
-    return(
-        <Title>
-            Main       
-        </Title>
+export default function Main() {
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositorios
+            </h1>
+            <Form onSubmit={()=>{}}>
+                <input type="text" placeholder="Adicione Repositorio"/>
+                <SumitButton>
+                    <FaPlus color='#FFF' size={14}/>
+                </SumitButton>
+            </Form>
+        </Container>
+
     )
 }
