d8e889ccf27b8c7445c48c3033f78545e004cbdc
unknown <itesp@PINCAPAO177.itesp.sp>
2021-07-28 15:12:52 -0300

    Carregando detalhes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Main from './pages/Main';
-import Repositorio from './pages/Main/Repositorio';
+import Repositorio from './pages/Repositorio';
 
 export default function Routes(){
     return(
