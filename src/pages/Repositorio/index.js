9626e67ab60c407d27dc6c5d2f1912e0f9d3dc53
Markilha <markilha@gmail.com>
2021-07-28 23:17:08 -0300

    Criando paginação

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,64 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Owner, Loading,BackButton,IssuesList } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles';
 import api from '../../services/api';
-import {FaArrowLeft} from 'react-icons/fa'
+import { FaArrowLeft } from 'react-icons/fa'
 
 export default function Repositorio({ match }) {
     const [repositorio, setRepositorio] = useState({});
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         async function load() {
             const nomeRepo = decodeURIComponent(match.params.repositorio);
             const [repositorioData, issuesData] = await Promise.all([
                 api.get(`/repos/${nomeRepo}`),
-                api.get(`/repos/${nomeRepo}/issues`,{
-                    params:{
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
                         state: 'open',
                         per_page: 5
                     }
                 })
             ]);
-           setRepositorio(repositorioData.data);          
-           setIssues(issuesData.data);
-           setLoading(false);          
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
         }
         load();
-    }, []);
+    }, [match.params.repositorio]);
 
-    if(loading){
-        return(
+    useEffect(() => {
+        async function loadIssue() {
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+            const response = await api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: 'open',
+                    page,
+                    per_page: 5,
+                },
+            });
+            setIssues(response.data);
+        }
+        loadIssue();
+    }, [match.params.repositorio, page]);
+
+    function handlePage(action) {
+        setPage(action === 'back' ? page - 1 : page + 1);
+
+    }
+
+    if (loading) {
+        return (
             <Loading>
                 <h1>Carregando...</h1>
             </Loading>
         )
     }
-    return (        
+    return (
         <Container>
             <BackButton to="/">
-                <FaArrowLeft color='#000' size={30}/>
+                <FaArrowLeft color='#000' size={30} />
             </BackButton>
             <Owner>
                 <img
-                 src={repositorio.owner.avatar_url} 
-                 alt={repositorio.owner.login}
-                 />
-                 <h1>{repositorio.name}</h1>
-                 <p>{repositorio.description}</p>
+                    src={repositorio.owner.avatar_url}
+                    alt={repositorio.owner.login}
+                />
+                <h1>{repositorio.name}</h1>
+                <p>{repositorio.description}</p>
             </Owner>
 
+
             <IssuesList>
-                {issues.map(issue=>(
+                {issues.map(issue => (
                     <li key={String(issue.id)}>
-                        <img 
-                        src={issue.user.avatar_url}
-                        alt={issue.user.login}
-                        /> 
+                        <img
+                            src={issue.user.avatar_url}
+                            alt={issue.user.login}
+                        />
                         <div>
                             <strong>
                                 <a href={issue.html_url}>{issue.title}</a>
-                                {issue.labels.map(label=>(
+                                {issue.labels.map(label => (
                                     <span key={String(label.id)}>{label.name}</span>
                                 ))}
                             </strong>
@@ -67,9 +89,17 @@ export default function Repositorio({ match }) {
                     </li>
 
                 ))}
-      
 
             </IssuesList>
+            <PageActions>
+                <button type='button' onClick={() => handlePage('back')}
+                    disabled={page < 2}>
+                    Voltar
+                </button>
+                <button type='button' onClick={() => handlePage('next')}>
+                    Próxima
+                </button>
+            </PageActions>
         </Container>
     )
 }
